b4be995e258ac7ddd9936488009c4841267baf3e
Sang Hyun Lee <you@example.com>
2021-09-23 01:11:42 -0400

    chessboard and pieces

---
+++ src/components/Tile/Tile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Tile.css';
+
+
+
+export default function Tile(props) {
+  const { number, image } = props;
+
+  if (number % 2 === 0) {
+    return (
+      <div className="tile black-tile">
+        <img src={image}></img>
+      </div>
+    )
+  } else {
+    return (
+      <div className="tile white-tile ">
+        <img src={image}></img>
+      </div>
+    )
+  }
+
+}
